08190d9bda3ab1262876912f0f7661e3c66e4675
Yu Luo <luoyu357@gmail.com>
2020-07-20 13:13:12 -0400

    upload the version 2 code

---
+++ ERPIDv2/src/indiana/edu/engine/OperationInstance.java
@@ -0,0 +1,10 @@
+package indiana.edu.engine;
+
+public class OperationInstance {
+	public static final String RESOLVE = "RESOLVE";
+	public static final String UPDATE = "UPDATE";
+	public static final String CREATE = "CREATE";
+	public static final String DELETE = "DELETE";
+	public static final String ANALYZE = "ANALYZE";
+	
+}
